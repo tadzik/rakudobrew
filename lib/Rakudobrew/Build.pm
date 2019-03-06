4f3a0edda599dd19382abd85e9bff588742cd4fc
Patrick Böker <patzim@users.noreply.github.com>
2019-03-06 01:14:17 +0100

    Split rakudobrew up into several files.

---
+++ lib/Rakudobrew/Build.pm
@@ -0,0 +1,135 @@
+package Rakudobrew::Build;
+require Exporter;
+our @ISA = qw( Exporter );
+our @EXPORT = qw(build_impl make build_triple build_zef);
+
+use strict;
+use warnings;
+use 5.010;
+use File::Spec::Functions qw(catdir updir);
+use Cwd qw(cwd);
+use Rakudobrew::Tools;
+use Rakudobrew::VersionHandling;
+use Rakudobrew::Variables;
+
+sub build_impl {
+    my ($impl, $ver, $configure_opts) = @_;
+    $ver //= 'master';
+    chdir $versions_dir;
+    unless (-d "$impl-$ver") {
+        for(@{$impls{$impl}{need_repo}}) {
+            update_git_reference($_);
+        }
+        run "$GIT clone --reference \"$git_reference/rakudo\" $git_repos{rakudo} $impl-$ver";
+    }
+    chdir "$impl-$ver";
+    run "$GIT fetch";
+    # of people say 'build somebranch', they usually mean 'build origin/somebranch'
+    my $ver_to_checkout = $ver;
+    eval {
+        run "$GIT rev-parse -q --verify origin/$ver";
+        $ver_to_checkout = "origin/$ver";
+    };
+    run "$GIT checkout $ver_to_checkout";
+
+    run $impls{$impl}{configure} . " $configure_opts";
+}
+
+sub make {
+    my $command = shift;
+    if(!-f 'Makefile') {
+        say STDERR "No Makefile found. Aborting.";
+        exit 1;
+    }
+    my $makefile = slurp('Makefile');
+    if($makefile =~ /^MAKE\s*=\s*(\w+)\s*$/m) {
+        my $make = $1;
+        run("$make $command");
+    }
+    else {
+        say STDERR "Couldn't determine correct make program. Aborting.";
+        exit 1;
+    }
+}
+
+sub build_triple {
+    my ($rakudo_ver, $nqp_ver, $moar_ver) = @_;
+    my $impl = "moar";
+    $rakudo_ver //= 'HEAD';
+    $nqp_ver //= 'HEAD';
+    $moar_ver //= 'HEAD';
+    chdir $versions_dir;
+    my $name = "$impl-$rakudo_ver-$nqp_ver-$moar_ver";
+    unless (-d $name) {
+        update_git_reference('rakudo');
+        run "$GIT clone --reference \"$git_reference/rakudo\" $git_repos{rakudo} $name";
+    }
+    chdir $name;
+    run "$GIT pull";
+    run "$GIT checkout $rakudo_ver";
+    if (-e 'Makefile') {
+        make('realclean');
+    }
+
+    unless (-d "nqp") {
+        update_git_reference('nqp');
+        run "$GIT clone --reference \"$git_reference/nqp\" $git_repos{nqp}";
+    }
+    chdir "nqp";
+    run "$GIT pull";
+    run "$GIT checkout $nqp_ver";
+
+    unless (-d "MoarVM") {
+        update_git_reference('MoarVM');
+        run "$GIT clone --reference \"$git_reference/MoarVM\" $git_repos{MoarVM}";
+    }
+    chdir "MoarVM";
+    run "$GIT pull";
+    run "$GIT checkout $moar_ver";
+    run "$PERL5 Configure.pl --prefix=" . catdir(updir(), updir(), 'install');
+    make('install');
+
+    chdir updir();
+    run "$PERL5 Configure.pl --backend=moar --prefix=" . catdir(updir(), 'install');
+    make('install');
+
+    chdir updir();
+    run "$PERL5 Configure.pl --backend=moar";
+    make('install');
+
+    if (-d 'zef') {
+        say "Updating zef as well";
+        build_zef($name);
+    }
+
+    return $name;
+}
+
+sub build_zef {
+    my $version = shift;
+    chdir catdir($versions_dir, $version);
+    unless (-d 'zef') {
+        run "$GIT clone $git_repos{zef}";
+    }
+    chdir 'zef';
+    run "$GIT pull -q";
+    run "$GIT checkout";
+    run which('perl6', $version) . " -Ilib bin/zef test .";
+    run which('perl6', $version) . " -Ilib bin/zef --/test --force install .";
+}
+
+sub update_git_reference {
+    my $repo = shift;
+    my $back = cwd();
+    print "Update git reference: $repo\n";
+    chdir $git_reference;
+    unless (-d $repo) {
+        run "$GIT clone $git_repos{$repo} $repo";
+    }
+    chdir $repo;
+    run "$GIT pull";
+    chdir $back;
+}
+
+1;
+
---
+++ lib/Rakudobrew/Variables.pm
@@ -0,0 +1,64 @@
+package Rakudobrew::Variables;
+require Exporter;
+our @ISA = qw( Exporter );
+our @EXPORT = qw( $brew_name $env_var $local_filename $prefix $versions_dir $shim_dir $git_reference $GIT $GIT_PROTO $PERL5 %git_repos %impls );
+
+use strict;
+use warnings;
+use 5.010;
+
+use FindBin qw($RealBin);
+use File::Spec::Functions qw(catfile catdir updir);
+
+our $brew_name = 'rakudobrew';
+our $env_var = 'PL6ENV_VERSION';
+our $local_filename = '.perl6-version';
+
+our $prefix = catdir($RealBin, updir());
+our $versions_dir = catdir($prefix, 'versions');
+our $shim_dir = catdir($prefix, 'shims');
+our $git_reference = catfile($prefix, 'git_reference');
+
+our $GIT       = $ENV{GIT_BINARY} // 'git';
+our $GIT_PROTO = $ENV{GIT_PROTOCOL} // 'git';
+our $PERL5     = $^X;
+
+sub get_git_url {
+    my ($protocol, $host, $user, $project) = @_;
+    if ($protocol eq "ssh") {
+        return "git\@${host}:${user}/${project}.git";
+    } else {
+        return "${protocol}://${host}/${user}/${project}.git";
+    }
+}
+
+our %git_repos = (
+    rakudo => get_git_url($GIT_PROTO, 'github.com', 'rakudo', 'rakudo'),
+    MoarVM => get_git_url($GIT_PROTO, 'github.com', 'MoarVM', 'MoarVM'),
+    nqp    => get_git_url($GIT_PROTO, 'github.com', 'perl6',  'nqp'),
+    zef    => get_git_url($GIT_PROTO, 'github.com', 'ugexe',  'zef'),
+);
+
+our %impls = (
+    jvm => {
+        name      => "jvm",
+        weight    => 20,
+        configure => "$PERL5 Configure.pl --backends=jvm --gen-nqp --git-reference=\"$git_reference\" --make-install",
+        need_repo => ['rakudo', 'nqp'],
+    },
+    moar => {
+        name      => "moar",
+        weight    => 30,
+        configure => "$PERL5 Configure.pl --backends=moar --gen-moar --git-reference=\"$git_reference\" --make-install",
+        need_repo => ['rakudo', 'nqp', 'MoarVM'],
+    },
+    'moar-blead' => {
+        name      => "moar-blead",
+        weight    => 35,
+        configure => "$PERL5 Configure.pl --backends=moar --gen-moar=master --gen-nqp=master --git-reference=\"$git_reference\" --make-install",
+        need_repo => ['rakudo', 'nqp', 'MoarVM'],
+    },
+);
+
+1;
+
