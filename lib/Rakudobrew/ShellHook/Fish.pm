74cce77a8bdbc906c1ec279a4683d859632584f3
Patrick Böker <patzim@users.noreply.github.com>
2019-04-12 23:59:25 +0200

    Implement support for the fish shell

---
+++ lib/Rakudobrew/ShellHook/Fish.pm
@@ -0,0 +1,102 @@
+package Rakudobrew::ShellHook::Fish;
+use strict;
+use warnings;
+use 5.010;
+use File::Spec::Functions qw(catdir splitpath);
+use FindBin qw($RealBin $RealScript);
+
+use Rakudobrew::Variables;
+use Rakudobrew::Tools;
+use Rakudobrew::VersionHandling;
+use Rakudobrew::ShellHook;
+use Rakudobrew::Build;
+
+sub get_init_code {
+    my $path = $ENV{PATH};
+    $path = Rakudobrew::ShellHook::clean_path($path, $RealBin);
+    $path =~ s/:/ /g;
+    $path = "$RealBin $path";
+    if (get_brew_mode() eq 'env') {
+        if (get_global_version() && get_global_version() ne 'system') {
+            $path = join(' ', get_bin_paths(get_global_version()), $path);
+        }
+    }
+    else { # get_brew_mode() eq 'shim'
+        $path = join(' ', $shim_dir, $path);
+    }
+
+    return <<EOT;
+set -x PATH $path
+
+function $brew_name
+    command $brew_name internal_hooked \$argv
+    and eval (command $brew_name internal_shell_hook Fish post_call_eval \$argv)
+end
+
+function _${brew_name}_is_not_register
+    set args (commandline -poc)
+    if [ (count \$args) -eq 3 -a \$args[1] = 'register' ]
+        return 1
+    else
+        return 0
+    end
+end
+
+complete -c $brew_name -f -n _${brew_name}_is_not_register -a '(command $brew_name internal_shell_hook Fish completions (commandline -poc) | string split " ")'
+EOT
+
+}
+
+sub post_call_eval {
+    Rakudobrew::ShellHook::print_shellmod_code('Bash', @_);
+}
+
+sub get_path_setter_code {
+    my $path = shift;
+    $path =~ s/:/ /g;
+    return "set -gx PATH $path";
+}
+
+sub get_shell_setter_code {
+    my $version = shift;
+    return "set -gx $env_var $version";
+}
+
+sub get_shell_unsetter_code {
+    return "set -ex $env_var";
+}
+
+sub completions {
+    my @words = @_;
+
+    if (@words == 1) {
+        my @commands = qw(version current versions list global switch shell local nuke unregister rehash list-available build register build-zef exec which whence mode self-upgrade triple test);
+        my $candidate = @words < 2 ? '' : $words[1];
+        say join(' ', grep({ substr($_, 0, length($candidate)) eq $candidate } @commands));
+    }
+    elsif(@words == 2 && ($words[1] eq 'global' || $words[1] eq 'switch' || $words[1] eq 'shell' || $words[1] eq 'local' || $words[1] eq 'nuke' || $words[1] eq 'test')) {
+        my @versions = get_versions();
+        push @versions, 'all'     if $words[1] eq 'test';
+        push @versions, '--unset' if $words[1] eq 'shell';
+        my $candidate = @words < 3 ? '' : $words[2];
+        say join(' ', grep({ substr($_, 0, length($candidate)) eq $candidate } @versions));
+    }
+    elsif(@words == 2 && $words[1] eq 'build') {
+        say join ' ', Rakudobrew::Build::available_backends(), 'all';
+    }
+    elsif(@words == 3 && $words[1] eq 'build') {
+        my @installed = get_versions();
+        my @installables = grep({ my $x = $_; !grep({ $x eq $_ } @installed) } Rakudobrew::Build::available_rakudos());
+
+        my $candidate = @words < 4 ? '' : $words[3];
+        say join(' ', grep({ substr($_, 0, length($candidate)) eq $candidate } @installables));
+    }
+    elsif(@words == 2 && $words[1] eq 'mode') {
+        my @modes = qw(env shim);
+        my $candidate = @words < 3 ? '' : $words[2];
+        say join(' ', grep({ substr($_, 0, length($candidate)) eq $candidate } @modes));
+    }
+}
+
+1;
+
